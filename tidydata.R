85706242d74863fd45c82cbdcd1df70499872cd7
Sateeshnallamothu <sknallamothu@hotmail.com>
2016-12-07 22:13:11 -0600

    3rd

--- tidydata.R
+++ tidydata.R
@@ -5,6 +5,7 @@ setwd('TidyData')
 # read common files.
 act.lbl<-fread('UCI HAR Dataset/activity_labels.txt')  ## activity labels. 
 cn<-fread('UCI HAR Dataset/features.txt')   ## read column/feature names
+
 # read test data.
 subject.test<-fread('UCI HAR Dataset/test/subject_test.txt')  ## test subject ids (1 to 30)
 tstx<-fread('UCI HAR Dataset/test/X_test.txt',col.names=cn$V2)  ## read test data with variables
@@ -15,10 +16,10 @@ subject.train<-fread('UCI HAR Dataset/train/subject_train.txt')  ## traiin subje
 trnx<-fread('UCI HAR Dataset/train/X_train.txt',col.names=cn$V2)  ## read train data with variables
 trny<-fread('UCI HAR Dataset/train/y_train.txt')  ## train label 
 
-#select features with 'mean' in it.   test data
+#select features with 'mean' and 'std' in it.   test data
 tstx.final<-tstx[,grep("mean\\(\\)|std\\(\\)|gravityMean\\)",colnames(tstx)),with=FALSE]    ## list of mean() cols
 
-#select features with 'mean' in it.   training data
+#select features with 'mean' and 'std' in it.   training data
 trnx.final<-trnx[,grep("mean\\(\\)|std\\(\\)|gravityMean\\)",colnames(trnx)),with=FALSE]    ## list of mean() cols
 
 # add activity and subject to test
@@ -29,6 +30,7 @@ trnx.final[,`:=`(Activity=act.lbl[trny$V1]$V2,Subject=subject.train$V1)]
 
 # merger both test and train into single data.table.
 master.data <- rbind(tstx.final,trnx.final)
+
 # quick check
 table(master.data$Subject)
 table(master.data$Activity)
@@ -36,11 +38,14 @@ table(master.data$Activity)
 # add meaningful names to columns/features. 
 colnames(master.data)<-gsub('^t',"time",colnames(master.data))
 colnames(master.data)<-gsub('^f',"frequency",colnames(master.data))
+
 # find mean of all features by Activity and by Suject. 
-tidy.summary<-master.data%>%
-  group_by(Activity,Subject)%>%
+tidy.summary<-master.data %>%
+  group_by(Activity,Subject) %>%
   summarize_all(mean)
+
 #print
 tidy.summary
+
 # write to a file.
 write.table(tidy.summary,file="tidysummary.txt",row.names = FALSE)
